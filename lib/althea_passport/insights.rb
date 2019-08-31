4d13b69b531fa9d82af7e54604e536ca51b4a724
Stephen Fiser <stephen@Stephens-MacBook-Pro.local>
2019-08-31 10:31:41 -0400

    disable insights

--- lib/althea_passport/insights.rb
+++ lib/althea_passport/insights.rb
@@ -19,26 +19,27 @@ module AltheaPassport
       # end
 
       def log_request(request)
-        data = {
-          time: Time.now.utc,
-          url: request.url,
-          service: AltheaPassport.configuration.service_name,
-          browser_info: request.env['HTTP_USER_AGENT'],
-          params: request.params,
-          session: request.session.to_hash
-        }
-
-        begin
-          AltheaPassport::ApiBase.call_althea_api(
-            :post,
-             request.session.to_hash[:token],
-            AltheaPassport.configuration.insights_url,
-            data
-          )
-        rescue => e
-          # need to do something here
-        end
+        # data = {
+        #   time: Time.now.utc,
+        #   url: request.url,
+        #   service: AltheaPassport.configuration.service_name,
+        #   browser_info: request.env['HTTP_USER_AGENT'],
+        #   params: request.params,
+        #   session: request.session.to_hash
+        # }
+        #
+        # begin
+        #   AltheaPassport::ApiBase.call_althea_api(
+        #     :post,
+        #      request.session.to_hash[:token],
+        #     AltheaPassport.configuration.insights_url,
+        #     data
+        #   )
+        # rescue => e
+        #
+        # end
       end
+      
     end
 
   end
--- lib/althea_passport/version.rb
+++ lib/althea_passport/version.rb
@@ -1,3 +1,3 @@
 module AltheaPassport
-  VERSION = '0.9.0'
+  VERSION = '0.9.1'
 end
